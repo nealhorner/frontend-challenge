32514ecd3373882e0ab4e96bba95cea3acdc17d6
Neal Horner <thenealhorner@gmail.com>
2024-05-04 12:38:08 -0700

    chore: Add route for homepage

--- server/app.js
+++ server/app.js
@@ -8,6 +8,9 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
