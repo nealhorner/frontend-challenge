0a31388853a40a28dd7dab25ff4b5db2220f09e4
Neal Horner <thenealhorner@gmail.com>
2024-05-04 12:30:34 -0700

    Sample server code

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const UserController = require('../controllers/UserController');
+
+// Define routes
+router.get('/', UserController.getAllUsers);
+router.get('/:id', UserController.getUserById);
+router.post('/', UserController.createUser);
+router.put('/:id', UserController.updateUser);
+router.delete('/:id', UserController.deleteUser);
+
+module.exports = router;
