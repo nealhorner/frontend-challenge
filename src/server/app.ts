d23e0b75eb9d705f5a3c9b8f30175bbeb35d8e78
Neal Horner <thenealhorner@gmail.com>
2024-05-04 17:55:37 -0700

    chore: Update dependencies and improve security

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,10 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
+import helmet from 'helmet';
 
 dotenv.config();
 
 const app = express();
+
+// Security
+app.use(helmet())
+app.disable('x-powered-by')
 
 // Middleware
 app.use(cors());
