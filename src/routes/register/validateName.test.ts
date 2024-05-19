56263fdc6de7e5e65d31e6ce1521919c8dd54277
Neal Horner <thenealhorner@gmail.com>
2024-05-19 00:04:15 -0700

    refactor: Improve email and password validation logic in registration form

--- src/routes/register/validateName.test.ts
+++ src/routes/register/validateName.test.ts
@@ -5,42 +5,70 @@ describe('validateName', () => {
   it('should return true for a valid name', () => {
     const name = 'John Doe';
     const result = validateName(name);
-    expect(result).toBe(true);
+    expect(result.invalidReason).toBeNull();
+    expect(result.isValid).toBe(true);
   });
 
   it('should return false for an empty name', () => {
     const name = '';
     const result = validateName(name);
-    expect(result).toBe(false);
+    expect(result.invalidReason).toBe('Names must be at least 1 character long');
+    expect(result.isValid).toBe(false);
+  });
+
+  it('should return false for a name longer than 50 characters', () => {
+    const name = 'John Doe'.repeat(10);
+    const result = validateName(name);
+    expect(result.invalidReason).toBe('Names cannot be more than 50 characters long');
+    expect(result.isValid).toBe(false);
   });
 
   it('should return false for a name with only spaces', () => {
     const name = '   ';
     const result = validateName(name);
-    expect(result).toBe(false);
+    expect(result.invalidReason).toBe('Names can not start with a space');
+    expect(result.isValid).toBe(false);
   });
 
   it('should return false for a name starting with a space', () => {
     const name = ' John Doe';
     const result = validateName(name);
-    expect(result).toBe(false);
+    expect(result.invalidReason).toBe('Names can not start with a space');
+    expect(result.isValid).toBe(false);
   });
 
   it('should return false for a name starting with a number', () => {
     const name = '1 John Doe';
     const result = validateName(name);
-    expect(result).toBe(false);
+    expect(result.invalidReason).toBe('Names can not start with numbers');
+    expect(result.isValid).toBe(false);
   });
 
   it('should return false for a name starting with a special character', () => {
     const name = '@John Doe';
     const result = validateName(name);
-    expect(result).toBe(false);
+    expect(result.invalidReason).toBe('Names cannot contain special characters');
+    expect(result.isValid).toBe(false);
   });
 
-  it('should return true for a name starting with a non-latin character', () => {
-    const name = '李 John Doe';
+  it('should return true for a name starting with a non-latin character - chinese ', () => {
+    const name = '伟祺';
     const result = validateName(name);
-    expect(result).toBe(true);
+    expect(result.invalidReason).toBeNull();
+    expect(result.isValid).toBe(true);
+  });
+
+  it('should return true for a name starting with a non-latin character - arabic', () => {
+    const name = 'جون باول'; // cspell:disable-line
+    const result = validateName(name);
+    expect(result.invalidReason).toBeNull();
+    expect(result.isValid).toBe(true);
+  });
+
+  it('should return false for a name containing an emoji', () => {
+    const name = 'John 😁 Doe';
+    const result = validateName(name);
+    expect(result.invalidReason).toBe('Names cannot contain emojis');
+    expect(result.isValid).toBe(false);
   });
 });
--- src/routes/register/validateName.ts
+++ src/routes/register/validateName.ts
@@ -1,17 +1,32 @@
 export const validateName = (name: string) => {
+  // name can not start with a space
+  if (name.match(/^\s/)) {
+    return { isValid: false, invalidReason: 'Names can not start with a space' };
+  }
+
   // Return false if name starts with a number
   if (name.match(/^\d/)) {
     return { isValid: false, invalidReason: 'Names can not start with numbers' };
   }
 
-  // Return false if name starts with a special character, but allow non-latin characters
-  if (name.match(/^[^a-zA-Z0-9]/) && name.match(/^[a-zA-Z0-9]/)) {
+  // Names can not contain emojis
+  if (name.match(/[\u{1F600}-\u{1F64F}]/gu)) {
+    return { isValid: false, invalidReason: 'Names cannot contain emojis' };
+  }
+
+  // Names can not contain special characters, like !@#$%^&*(), but allow non-latin characters, like chinese or arabic characters
+  if (name.match(/[^a-zA-Z0-9\s\u4E00-\u9FA5\u0600-\u06FF]/)) {
     return { isValid: false, invalidReason: 'Names cannot contain special characters' };
   }
 
-  if (name.length >= 1) {
+  // Names can not be more than 50 characters long
+  if (name.length > 50) {
+    return { isValid: false, invalidReason: 'Names cannot be more than 50 characters long' };
+  }
+
+  if (name.length <= 1) {
     return { isValid: false, invalidReason: 'Names must be at least 1 character long' };
   }
 
-  return { isValid: true, invalidReason: '' };
+  return { isValid: true, invalidReason: null };
 };
