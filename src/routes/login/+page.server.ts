53f42ccad3f629f0c381c801b34d95e848f10676
Neal Horner <thenealhorner@gmail.com>
2024-05-21 23:33:11 -0700

    refactor: Improve login page with login form validation
    This commit adds form validation to the login page, ensuring that both the username and password fields are required. It also displays error messages when the fields are empty or invalid. This change improves the user experience by providing feedback and preventing invalid form submissions.

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,55 @@
+import { fail, redirect } from '@sveltejs/kit';
+import bcrypt from 'bcrypt';
+import type { Action, Actions } from './$types';
+import prisma from '$lib/prisma';
+
+const login: Action = async ({ cookies, request }) => {
+  const data = await request.formData();
+  const username = data.get('username');
+  const password = data.get('password');
+
+  if (typeof username !== 'string' || typeof password !== 'string' || !username || !password) {
+    return fail(400, { invalid: true });
+  }
+
+  const email = username; // FIXME this is a hack to make the code work
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    // TODO Add delay to prevent brute force attacks
+    return fail(400, { credentials: true });
+  }
+
+  const userPassword = await bcrypt.compare(password, user.hashedPassword);
+
+  if (!userPassword) {
+    // TODO Add delay to prevent brute force attacks
+    return fail(400, { credentials: true });
+  }
+
+  // generate new auth token just in case
+  const authenticatedUser = await prisma.user.update({
+    where: { email: user.email },
+    data: { userAuthToken: crypto.randomUUID() }
+  });
+
+  cookies.set('session', authenticatedUser.userAuthToken, {
+    // send cookie for every page
+    path: '/',
+    // server side only cookie so you can't use `document.cookie`
+    httpOnly: true,
+    // only requests from same site can send cookies
+    // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
+    sameSite: 'strict',
+    // only sent over HTTPS in production
+    secure: process.env.NODE_ENV === 'production',
+    // set cookie to expire after a month
+    maxAge: 60 * 60 * 24 * 30
+  });
+
+  // redirect the user
+  redirect(302, '/');
+};
+
+export const actions: Actions = { login };
