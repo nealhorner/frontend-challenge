7e194c65f364a1153a911c4a87fb9a76cf02a91f
Neal Horner <thenealhorner@gmail.com>
2024-10-10 23:49:02 -0700

    feat: Add Prompter component to quiz page

---
+++ src/routes/quiz/components/prompter.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Question from '$lib/components/Question.svelte';
+
+  export let quizData;
+
+  let isComplete = quizData.completedQuestions.length === quizData.questions.length;
+  let currentQuestionId: number =
+    quizData.questions.find((q: number) => !quizData.completedQuestions.includes(q)) ??
+    quizData.questions[quizData.questions.length - 1];
+</script>
+
+{#if isComplete}
+  <p>Finished Quiz</p>
+{:else}
+  <p>
+    the current question is: {currentQuestionId}
+    Question: <Question questionId={currentQuestionId} />
+  </p>
+{/if}
