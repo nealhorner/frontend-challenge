369ec3bb78051c62508cc47a24fde38ead348680
Neal Horner <thenealhorner@gmail.com>
2025-01-25 01:13:17 -0800

    Refactor package.json: Remove trailing newline at end of file Refactor hooks.server.ts: Simplify handle function Remove auth-utilities.ts: Delete unused file

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,3 @@
-import { sequence } from '@sveltejs/kit/hooks';
 import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 import { PUBLIC_SENTRY_DSN } from '$env/static/public';
@@ -12,7 +11,7 @@ Sentry.init({
 });
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-export const handle = sequence(sentryHandle());
+export const handle = sentryHandle();
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
 export const handleError = handleErrorWithSentry();
