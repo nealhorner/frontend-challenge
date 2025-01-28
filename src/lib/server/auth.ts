771c71381e41892c2ac69805c5249165ddaad6f6
Neal Horner <thenealhorner@gmail.com>
2025-01-27 23:24:51 -0800

    Refactor server authentication handling and session management

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,87 @@
+import type { RequestEvent } from '@sveltejs/kit';
+import { sha256 } from '@oslojs/crypto/sha2';
+import { encodeBase64url, encodeHexLowerCase } from '@oslojs/encoding';
+import prisma from '$lib/prisma';
+
+const DAY_IN_MS = 1000 * 60 * 60 * 24;
+
+export const sessionCookieName = 'auth-session';
+
+export function generateSessionToken() {
+  const bytes = crypto.getRandomValues(new Uint8Array(18));
+  const token = encodeBase64url(bytes);
+  return token;
+}
+
+export async function createSession(token: string, userId: string) {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+
+  const newSession = await prisma.userSession.create({
+    data: {
+      id: sessionId,
+      userId: userId,
+      expiresAt: new Date(Date.now() + DAY_IN_MS * 30)
+    }
+  });
+  return newSession;
+}
+
+export async function validateSessionToken(token: string) {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const session = await prisma.userSession.findUnique({
+    where: { id: sessionId },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true
+        }
+      }
+    }
+  });
+
+  if (!session) {
+    return { session: null, user: null };
+  }
+  const { user } = session;
+
+  const sessionExpired = Date.now() >= session.expiresAt.getTime();
+  if (sessionExpired) {
+    await prisma.userSession.delete({
+      where: { id: session.id }
+    });
+    return { session: null, user: null };
+  }
+
+  const renewSession = Date.now() >= session.expiresAt.getTime() - DAY_IN_MS * 15;
+  if (renewSession) {
+    session.expiresAt = new Date(Date.now() + DAY_IN_MS * 30);
+    await prisma.userSession.update({
+      where: { id: session.id },
+      data: { expiresAt: session.expiresAt }
+    });
+  }
+
+  return { session, user };
+}
+
+export type SessionValidationResult = Awaited<ReturnType<typeof validateSessionToken>>;
+
+export async function invalidateSession(sessionId: string) {
+  await prisma.userSession.delete({
+    where: { id: sessionId }
+  });
+}
+
+export function setSessionTokenCookie(event: RequestEvent, token: string, expiresAt: Date) {
+  event.cookies.set(sessionCookieName, token, {
+    expires: expiresAt,
+    path: '/'
+  });
+}
+
+export function deleteSessionTokenCookie(event: RequestEvent) {
+  event.cookies.delete(sessionCookieName, {
+    path: '/'
+  });
+}
