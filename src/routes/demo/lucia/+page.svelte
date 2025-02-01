f632f68435967239fbdccf2c837470fecd420372
Neal Horner <thenealhorner@gmail.com>
2025-02-01 10:46:05 -0800

    Add Lucia demo pages and server logic

---
+++ src/routes/demo/lucia/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+  import type { PageServerData } from './$types';
+
+  export let data: PageServerData;
+</script>
+
+<h1>Hi, {data.user.username}!</h1>
+<p>Your user ID is {data.user.id}.</p>
+<form method="post" action="?/logout" use:enhance>
+  <button>Sign out</button>
+</form>
