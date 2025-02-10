c997fe0ae24adc0a3065ec100222dd498abc3e76
Neal Horner <thenealhorner@gmail.com>
2025-02-09 22:24:52 -0800

    Remove unused HTML utility functions and related tests for cleaner codebase

--- src/lib/utilities/filterHTML.ts
+++
@@ -1,8 +0,0 @@
-export const filterHTML = (html: string) => {
-  // if html contains &lt; or &gt, it's probably not safe
-  if (html.includes('&lt;') || html.includes('&gt;')) {
-    console.log('Filtering unsafe HTML', html);
-    return '';
-  }
-  return html;
-};
--- src/routes/learn/+page.server.ts
+++ src/routes/learn/+page.server.ts
@@ -1,21 +1,10 @@
 import prisma from '$lib/prisma';
 import type { PageServerLoad } from './$types';
-import { decodeHTML } from '$lib/utilities/decodeHTML';
-import { cleanUnsafeHTML } from '$lib/utilities/cleanUnsafeHTML';
 
 export const load = (async () => {
   try {
     const learning_resources = await prisma.learningResources.findMany();
-
-    // Sanitize the description
-    learning_resources.forEach((learning_resource) => {
-      if (learning_resource.description) {
-        const decodedHTML = decodeHTML(learning_resource.description);
-        learning_resource.description = cleanUnsafeHTML(decodedHTML);
-      }
-    });
-
-    return { learning_resources };
+    return learning_resources;
   } catch (error) {
     // Handle the error here
     console.error('Error retrieving learning resources:', error);
